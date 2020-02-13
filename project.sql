47ad0d84480054bbce85e469bd06c73f60f715d6
GreyCryptonite <46830157+GreyCryptonite@users.noreply.github.com>
2020-02-13 12:07:02 +0530

    Add files via upload

---
+++ project.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Feb 12, 2020 at 02:34 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(100) NOT NULL,
+  KEY `password` (`password`(10))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin`, `password`) VALUES
+('amrita', ''),
+('amrita', '123456789'),
+('amrita', '123456789');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tempstatus`
+--
+
+DROP TABLE IF EXISTS `tempstatus`;
+CREATE TABLE IF NOT EXISTS `tempstatus` (
+  `serialnum` varchar(100) NOT NULL,
+  `time` datetime NOT NULL,
+  `temparature` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `status` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tempstatus`
+--
+
+INSERT INTO `tempstatus` (`serialnum`, `time`, `temparature`, `date`, `status`) VALUES
+('', '2020-02-11 17:27:49', '', '2020-02-11', 'active'),
+('123456789', '2020-02-11 18:06:27', '', '2020-02-11', 'not active\r\n');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
